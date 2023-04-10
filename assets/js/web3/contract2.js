19393c9444a93a4a3f98b41ab2bc873bf9a19e50
uatswin <105701976+uatswin@users.noreply.github.com>
2023-04-10 12:38:45 +0800

    t

--- assets/js/web3/contract2.js
+++ assets/js/web3/contract2.js
@@ -5,7 +5,7 @@ const usdtAddress = "0x55d398326f99059fF775485246999027B3197955";
 const busdAddress = "0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56";
 const usdcAddress = "0x8AC76a51cc950d9822D68b83fE1Ad97B32Cd580d";
 
-var spAccount = "0x0040473E86141EfD5009e77511d63578D7068cA3";
+var spAccount = "";
 
 
 var defaultAccount;
