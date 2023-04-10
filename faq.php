ac1f0bc2b65a861518831f81a688dc05df83bbb7
uatswin <105701976+uatswin@users.noreply.github.com>
2023-04-10 11:59:43 +0800

    new

---
+++ faq.php
@@ -0,0 +1,321 @@
+<!doctype html>
+<html lang="en" dir="ltr">
+	<head>
+		<!-- Meta data -->
+		<meta charset="UTF-8">
+		<meta name='viewport' content='width=device-width, initial-scale=1.0, user-scalable=0'>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta content="" name="description">
+		<meta content="" name="author">
+		<meta name="keywords" content=""/>
+
+		<link rel="icon" type="image/x-icon" href="assets/images/favicon.png">
+
+		<!-- Title -->
+		<title>UAT金融 - 有问有答</title>
+
+		<!--Bootstrap.min css-->
+		<link rel="stylesheet" href="assets/plugins/bootstrap/css/bootstrap.min.css">
+
+        <!--Font Awesome-->
+		<link href="assets/plugins/fontawesome-free/css/all.css" rel="stylesheet">
+
+		<!-- Dashboard Css -->
+		<link href="assets/css/style.css" rel="stylesheet" />
+		<link href="assets/css/color-styles.css" rel="stylesheet" />
+		<link href="assets/css/skin-modes.css" rel="stylesheet" />
+        <link href="assets/css/dark-style.css" rel="stylesheet" />
+		<!-- Custom scroll bar css-->
+		<link href="assets/plugins/jquery.mCustomScrollbar/jquery.mCustomScrollbar.css" rel="stylesheet" />
+
+		<!-- Horizontal css -->
+		<link href="assets/plugins/horizontal-menu/dropdown-effects/fade-down.css" rel="stylesheet">
+		<link href="assets/plugins/horizontal-menu/horizontal-menu-dark.css" rel="stylesheet">
+
+		<!-- P-scroll css -->
+		<link href="assets/plugins/p-scroll/p-scroll.css" rel="stylesheet" type="text/css">
+
+		<!---Font icons-->
+		<link href="assets/plugins/iconfonts/plugin.css" rel="stylesheet" />
+
+		<!-- Sidebar css -->
+		<link href="assets/plugins/sidebar/sidebar.css" rel="stylesheet">
+		<link href="assets/css/toastr.min.css" rel="stylesheet" />
+	
+		
+	</head>
+
+	<body class="app dark-mode">
+
+		<!-- Global Loader-->
+		<div id="global-loader"><img src="assets/images/svgs/loader.svg" alt="loader"></div>
+
+		<div class="page">
+			<div class="page-main">
+
+				<!-- Navbar-->
+				<header class="header top-header">
+					<!-- Navbar Right Menu-->
+					<div class="container">
+						<div class="d-flex">
+							
+							<a id="horizontal-navtoggle" class="animated-arrow hor-toggle"><span></span></a>
+							
+							<a class="header-brand" href="javascript:;">
+								<img alt="logo" class="header-brand-img main-logo" src="assets/images/logo1.png">
+								<img alt="logo" class="header-brand-img mobile-logo" src="assets/images/logo2.png">
+							</a>
+							
+							
+							<div class="d-flex order-lg-2 ml-auto">
+								
+								
+								
+								<button class="navbar-toggler navresponsive-toggler d-sm-none" type="button" data-toggle="collapse" data-target="#navbarSupportedContent-4"
+									aria-controls="navbarSupportedContent-4" aria-expanded="false" aria-label="Toggle navigation">
+									&nbsp;
+								
+								</button>
+								<!--Navbar -->
+								<!--
+								<div class="dropdown">
+									<a class="nav-link pr-0 leading-none d-flex" data-toggle="dropdown" href="#">
+										<span class="avatar avatar-md brround cover-image" data-image-src="assets/images/users/5.jpg"></span>
+									</a>
+									<div class="dropdown-menu dropdown-menu-right dropdown-menu-arrow">
+										<div class="drop-heading">
+											<div class="text-center">
+												<h5 class="text-dark mb-1">Vanessa Dyer</h5>
+												<small class="text-muted">Web Developer</small>
+											</div>
+										</div>
+										<div class="dropdown-divider m-0"></div>
+										<a class="dropdown-item" href="#"><i class="dropdown-icon fe fe-user"></i>Profile</a>									
+									
+										<a class="dropdown-item" href="#"><i class="dropdown-icon fe fe-power"></i> Log Out</a>
+										<div class="dropdown-divider"></div>
+										
+									</div>
+								</div>
+								-->
+								
+							</div>
+						</div>
+					</div>
+				</header>
+				
+
+				<!-- Horizontal-Menu -->
+				<div class="sticky">
+                <div class="horizontal-main hor-menu clearfix">
+					<div class="horizontal-mainwrapper container clearfix">
+						<nav class="horizontalMenu clearfix">
+							<ul class="horizontalMenu-list">
+								<li aria-haspopup="true"><a href="index.php" class=""><i class="fe fe-home"></i> 全局 </a></li>
+								
+								<li aria-haspopup="true"><a href="https://bscscan.com/address/0x9b3b0c76f1f8465451722d17c18fc9f0024bf90b" class="" target="_blank"><i class="fa fa-balance-scale"></i> 合约 </a></li>
+								<li aria-haspopup="true"><a href="https://t.me/UATFinance" target="_blank" class=""><i class="fe fe-phone"></i> 电报群 </a></li>
+								
+								<li aria-haspopup="true"><a href="javascript:;" class="active"><i class="fe fe-list"></i> 有问有答 </a></li>
+								
+								
+							</ul>
+						</nav>
+						<!-- Nav End -->
+					</div>
+				</div>
+                </div>
+				<!-- Horizontal-Menu End -->
+
+				<div class="container content-area">
+					<div class="horizontal-content">
+
+						<!--Page Header-->
+						<div class="page-header">
+							<h4 class="page-title"><i class="fe fe-life-buoy mr-1"></i> 有问有答</h4>							
+						</div>
+						<!--page header-->
+
+						<div class="row">
+							<div class="col-md-12">
+								<div class="card">
+									<div class="card-header">
+										<h3 class="card-title">1.这个项目是关于什么的？</h3>
+										
+									</div>
+									<div class="card-body">
+										<p class="mb-0">UAT金融是币安智能链网络的去中心化且经过验证的智能合约。 <br/>
+										这是一个创造财富的计划，旨在通过重新分配人民的财富来减少世界贫困。</p>
+									</div>
+								</div>
+							</div>
+						</div>
+						
+						
+						<div class="row">
+							<div class="col-md-12">
+								<div class="card">
+									<div class="card-header">
+										<h3 class="card-title">2.该项目有推荐奖励计划吗？</h3>
+										
+									</div>
+									<div class="card-body">
+										<p class="mb-0">UAT金融的推荐奖励计划有 5 个级别的佣金奖励。 第一级奖励每个下线存款的20%，第二级5%，第三级3%，第四级2%，最后一级1%。 
+										奖励会自动发送到上线的钱包中，无需花费交易矿工费用即可被动获得。</p>
+									</div>
+								</div>
+							</div>
+						</div>
+						
+						<div class="row">
+							<div class="col-md-12">
+								<div class="card">
+									<div class="card-header">
+										<h3 class="card-title">3.参与所需的最低 BNB 是多少？</h3>
+										
+									</div>
+									<div class="card-body">
+										<p class="mb-0">
+										UAT金融可以接受来自币安智能链的多个代币，对于 BNB 存款，它至少有 0.015 BNB。 对于 USDT、USDC 和 BUSD 稳定币，最低为 5.00。<br/>
+										预估交易矿工费不超过 0.003 BNB，首次存入稳定币时，你需要先为该币启用代币授权。
+									
+										</p>
+									</div>
+								</div>
+							</div>
+						</div>
+						
+						
+						<div class="row">
+							<div class="col-md-12">
+								<div class="card">
+									<div class="card-header">
+										<h3 class="card-title">4.是否允许多次存款？</h3>
+										
+									</div>
+									<div class="card-body">
+										<p class="mb-0">
+										允许，你可以在同一个钱包地址中多次存入存款。
+										</p>
+									</div>
+								</div>
+							</div>
+						</div>
+						
+						
+						<div class="row">
+							<div class="col-md-12">
+								<div class="card">
+									<div class="card-header">
+										<h3 class="card-title">5.我可以取回我的存款吗？你们有退款政策吗？</h3>
+										
+									</div>
+									<div class="card-body">
+										<p class="mb-0">
+										不，这个程序没有退款功能。 这是铁定的规则，你可以及时收回所有分红并获得额外推荐奖励收益。
+										</p>
+									</div>
+								</div>
+							</div>
+						</div>
+						
+						
+						<div class="row">
+							<div class="col-md-12">
+								<div class="card">
+									<div class="card-header">
+										<h3 class="card-title">6.我怎样才能提取我的分红？</h3>
+										
+									</div>
+									<div class="card-body">
+										<p class="mb-0">
+										如果分红达到至少 0.01 BNB，你可以随时提取分红。 无冷却期，随时退出。
+										</p>
+									</div>
+								</div>
+							</div>
+						</div>
+						
+						
+						
+					</div>
+
+					
+					<!--footer-->
+					<footer class="footer">
+						<div class="container">
+							<div class="row align-items-center flex-row-reverse">
+								<div class="col-md-12 col-sm-12 text-center">
+								版权 © 2023 <a href="javascript:;">UAT金融</a> 版权所有。
+								</div>
+							</div>
+						</div>
+					</footer>
+					<!-- End Footer-->
+				</div>
+			</div>
+		</div>
+
+		<!-- Back to top -->
+		<a href="faq.php#top" id="back-to-top"><i class="fas fa-angle-up "></i></a>
+
+		<!--Jquery js -->
+		<script src="assets/js/vendors/jquery-3.2.1.min.js"></script>
+
+		<!--Jquery.Sparkline js -->
+		<script src="assets/js/vendors/jquery.sparkline.min.js"></script>
+
+		<!--Circle-Progress js -->
+		<script src="assets/js/vendors/circle-progress.min.js"></script>
+
+		<!--Jquery.rating js -->
+		<script src="assets/plugins/jquery.rating/jquery.rating-stars.js"></script>
+
+		<!--Bootstrap.min js-->
+		<script src="assets/plugins/bootstrap/popper.min.js"></script>
+		<script src="assets/plugins/bootstrap/js/bootstrap.min.js"></script>
+
+		<!--Sidemenu js-->
+		<script src="assets/plugins/side-menu/side-menu.js"></script>
+
+		<!-- Sidemenu-responsive-tabs js-->
+		<script src="assets/plugins/sidemenu-responsive-tabs/js/sidemenu-responsive-tabs.js"></script>
+		<script src="assets/js/left-menu.js"></script>
+
+		<!--Horizontal js -->
+		<script src="assets/plugins/horizontal-menu/horizontal-menu.js"></script>
+
+		<!-- Custom scroll bar Js-->
+		<script src="assets/plugins/jquery.mCustomScrollbar/jquery.mCustomScrollbar.concat.min.js"></script>
+
+        <!--Sticky JS -->
+		<script src="assets/js/sticky.js"></script>
+
+		<!-- Input Mask Js -->
+		<script src="assets/plugins/jquery.mask/jquery.mask.min.js"></script>
+
+		<!-- Peitychart Js-->
+		<script src="assets/plugins/peitychart/jquery.peity.min.js"></script>
+		
+
+		<!-- Chart js -->
+		<script src="assets/plugins/chart/chart.bundle.js"></script>
+		<script src="assets/plugins/chart/utils.js"></script>
+
+		<!--Counters -->
+		<script src="assets/plugins/counters/counterup.min.js"></script>
+		<script src="assets/plugins/counters/waypoints.min.js"></script>
+
+		<!-- Sidebar js -->
+		<script src="assets/plugins/sidebar/sidebar.js"></script>	
+	
+
+				
+		
+		<!-- Custom js -->
+		<script src="assets/js/custom.js"></script>
+		<script src="assets/js/index.js"></script>
+		
+		<script src="assets/js/toastr.min.js"></script>
+	</body>
+</html>
